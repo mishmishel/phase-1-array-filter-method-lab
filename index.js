bec3fb46ec81cf6195320d3840a4037db57f9fd2
Mishel Tyurkova <misheltyurkova@gmail.com>
2023-05-27 12:26:53 +1000

    Finished Lab

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution here
+
+const drivers = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+
+const findMatching = (arr, query) => {
+    return arr.filter(element => 
+    element.toLowerCase().indexOf(query.toLowerCase()) !== -1)
+}
+console.log(filterItems(drivers, "Bobby"));
+
+function fuzzyMatch(array, query){
+    return array.filter(x => x.startsWith(query));
+} 
+
+function matchName(array, query) {
+    return array.filter(driver => driver.name === query);
+}
+
+
